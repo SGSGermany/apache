8f7113cdf87e47dbadd65464016e1689981539d5
Daniel Rudolf <github.com@daniel-rudolf.de>
2025-05-22 17:21:04 +0200

    entrypoint.sh: Ignore SSL config if Apache isn't running with 'ssl' module

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -14,16 +14,19 @@ set -e
 
 [ $# -gt 0 ] || set -- httpd-foreground
 if [ "$1" == "httpd-foreground" ]; then
-    # generate Diffie Hellman parameters, if necessary
-    if [ ! -f "/etc/apache2/ssl/dhparams.pem" ]; then
-        # generating Diffie Hellman parameters might take a few minutes...
-        printf "[%s] [entrypoint:notice] Generating Diffie Hellman parameters\n" \
-            "$(LC_ALL=C.UTF-8 date +'%a %b %d %T.000000 %Y')" >&2
-        openssl dhparam -out "/etc/apache2/ssl/dhparams.pem" 2048
-    fi
+    # manage SSL config, if module is enabled
+    if a2querymod -q "ssl"; then
+        # generate Diffie Hellman parameters, if necessary
+        if [ ! -f "/etc/apache2/ssl/dhparams.pem" ]; then
+            # generating Diffie Hellman parameters might take a few minutes...
+            printf "[%s] [entrypoint:notice] Generating Diffie Hellman parameters\n" \
+                "$(LC_ALL=C.UTF-8 date +'%a %b %d %T.000000 %Y')" >&2
+            openssl dhparam -out "/etc/apache2/ssl/dhparams.pem" 2048
+        fi
 
-    # start SSL certificate watchdog
-    apache-cert-watchdog &
+        # start SSL certificate watchdog
+        apache-cert-watchdog &
+    fi
 
     exec "$@"
 fi
